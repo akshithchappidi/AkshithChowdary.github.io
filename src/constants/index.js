ab964484cd10b3d306aa551ebe58666f850b0da7
akshithchappidi <60094672+akshithchappidi@users.noreply.github.com>
2023-09-09 02:19:44 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -152,24 +152,24 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
+        "I will updated the testimonals soon.",
+      name: "XYZ",
       designation: "CFO",
-      company: "Acme Co",
+      company: "ABC",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
+        "I will updated the testimonals soon.",
+      name: "XYZ",
       designation: "COO",
       company: "DEF Corp",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
+        "I will updated the testimonals soon.",
+      name: "XYZ",
       designation: "CTO",
       company: "456 Enterprises",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
